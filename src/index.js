4b1c8d815c37915c7bc136aaea50bf2afa51218a
devulogi <devulogijrp.github@gmail.com>
2021-02-02 15:19:53 +0800

    fresh start for blogposts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
-import App from "./App";
+import App from "./components/App";
 
-import { createTodo } from "./actions";
-import store from "./reducers";
-
-store.dispatch(createTodo({ todo: "learn react" }));
-store.dispatch(createTodo({ todo: "learn redux" }));
-store.dispatch(createTodo({ todo: "learn hooks" }));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
